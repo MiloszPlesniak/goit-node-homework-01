00cb1a90286ba1e659b37136dfe1af64c9734d33
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2023-03-06 06:16:06 +0100

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  const contactsList = await fs.readFile(contactsPath);
+  //   console.log(JSON.parse(contactsList));
+  return JSON.parse(contactsList);
+}
+
+async function getContactById(contactId) {
+  const contactsList = await listContacts();
+  const foundContact = contactsList.find((contact) => contact.id == contactId);
+  return foundContact;
+}
+
+async function removeContact(contactId) {
+  const contactsList = await listContacts();
+  const index = contactsList.findIndex((contact) => contact.id == contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [contact] = contactsList.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const contactsList = await listContacts();
+  const newContact = {
+    id: Number(contactsList[contactsList.length - 1].id) + 1,
+    name,
+    email,
+    phone,
+  };
+
+  contactsList.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contactsList));
+  return newContact;
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+const { Command } = require("commander");
+const program = new Command();
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts.js");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refaktor
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.table(contact);
+      break;
+
+    case "add":
+      const addedContact = await addContact(name, email, phone);
+      console.table(addedContact);
+      break;
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.table(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
